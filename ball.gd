018e6c686e9e763f5405338d0d932da3d24c4f0f
luke-hc-jackson <luke.hc.jackson@gmail.com>
2025-07-27 14:24:19 +0100

    Add ball launch direction control and arrow indicator

--- ball.gd
+++ ball.gd
@@ -1,34 +1,52 @@
 extends CharacterBody2D
 
 @export var initial_speed = 200
+var speed
 
 ##The proportion of initial_speed added to speed every bounce
 @export var bounce_speedup = 0.1
-
 @export var bounce_randomness = 0.5
 
-var speed
+var launched
+var offset = Vector2(0, -30)
+var rotationSensitivity = 0.05
+var rotationClamp = deg_to_rad(45)
+
 
 func _ready():
-	speed = initial_speed
-	#velocity = Vector2.ZERO
-	velocity = Vector2(0, 1) * speed
-	
-func _process(delta: float) -> void:
-	
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		#print(collision.get_collider().name)
-		#print(collision.get_collider().collision_layer)
-		
-		
-		var normal = collision.get_normal()
-		#todo fix this - for higher speeds, have to increase bounce randomness to compensate
-		#but applying the random v2 to the normalised .bounce makes it completely unpredictable so don't do that
-		velocity = (velocity.bounce(normal) + 
-				   Vector2(randf_range(-bounce_randomness, bounce_randomness), 
-						   randf_range(-bounce_randomness, bounce_randomness))).normalized() * speed
+	readyToLaunch()
 
-		if collision.get_collider().is_in_group("brick"):
-			collision.get_collider().call("hit")
-			speed += initial_speed * bounce_speedup
+func readyToLaunch():
+	speed = initial_speed
+	velocity = Vector2.ZERO
+	launched = false
+	
+func launch():
+	launched = true
+	$RotationAnchor.hide()
+	velocity = Vector2.from_angle($RotationAnchor.rotation) * speed
+	
+func followPaddle(pos):
+	print(pos)
+	position = pos + offset
+	
+func rotateTheBall(dir):
+	print(dir)
+	$RotationAnchor.rotation += dir * rotationSensitivity
+	$RotationAnchor.rotation = clamp($RotationAnchor.rotation, -rotationClamp, rotationClamp)
+
+func _process(delta: float) -> void:
+	if launched:
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			#BOUNCE
+			var normal = collision.get_normal()
+			#todo fix this - for higher speeds, have to increase bounce randomness to compensate
+			#but applying the random v2 to the normalised .bounce makes it completely unpredictable so don't do that
+			velocity = (velocity.bounce(normal) + 
+					   Vector2(randf_range(-bounce_randomness, bounce_randomness), 
+							   randf_range(-bounce_randomness, bounce_randomness))).normalized() * speed
+			#BRICK HIT
+			if collision.get_collider().is_in_group("brick"):
+				collision.get_collider().call("hit")
+				speed += initial_speed * bounce_speedup
--- player.gd
+++ player.gd
@@ -4,8 +4,14 @@ extends CharacterBody2D
 var screen_size
 var width_of_side_bars = 121
 
+var ballLaunched
+signal launchTheBall
+signal playerPosition(currentPos: Vector2)
+signal ballRotation(dir:int)
+
 func _ready():
 	screen_size = get_viewport_rect().size
+	ballLaunched = false
 	
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
@@ -18,9 +24,22 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0:
 		#don't actually think i need to normalise but whatever
 		velocity = velocity.normalized() * speed
-		
-	#position += velocity * delta
-	#position = position.clamp(Vector2(0 + width_of_side_bars, 0), 
-	#						  Vector2(screen_size.x - width_of_side_bars, screen_size.y))
 	
 	var collision = move_and_collide(velocity * delta)
+	
+	if !ballLaunched:
+		#either shoot the ball or move it relative to the paddle position
+		if Input.is_action_pressed("shoot"):
+			ballLaunched = true
+			launchTheBall.emit()
+		else:
+			emit_signal("playerPosition", position)
+			
+		#if the ball has not been shot, we can rotate the direction it will fire
+		var ballRotationDirection = 0
+		if Input.is_action_pressed("turn_left"):
+			ballRotationDirection -= 1
+		if Input.is_action_pressed("turn_right"):
+			ballRotationDirection += 1
+		if ballRotationDirection != 0:
+			emit_signal("ballRotation", ballRotationDirection)
